f3a9a11767c6f6ead7ee8c4a6399d0b3b8e7a7c6
Steve Deane Jr <wildmechanic@gmail.com>
2021-02-28 18:13:10 -0700

    Stuffs

--- app/src/main/java/com/netwokz/linkednotes/MainActivity.java
+++ app/src/main/java/com/netwokz/linkednotes/MainActivity.java
@@ -2,13 +2,16 @@ package com.netwokz.linkednotes;
 
 import android.os.Bundle;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +23,20 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = null;
     String TAG = "MainActivity";
+    String LIST = "list";
+    Integer currentListCountID;
+    DocumentReference myGroceryList;
+    Map<String, Object> groceryList = null;
+    String[] foodArray = {"Bread", "Milk", "Beer", "Cheese", "Paper Towels", "Plates", "Cereal", "Water", "Steak", "Hot Dogs"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,54 +45,76 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (db == null) {
+            initializeDataBase();
+        }
+        retreiveDB();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 testDBEntry();
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
 
-    public void initializeDataBase(){
+    public void initializeDataBase() {
         // Access a Cloud Firestore instance from your Activity
         FirebaseApp.initializeApp(this);
         db = FirebaseFirestore.getInstance();
+        myGroceryList = db.collection("Lists").document("Grocery's");
+        // Retrieve list and IDs, set counter accordingly
+        retreiveDB();
+        currentListCountID = 0;
     }
 
-    public void testDBEntry(){
-        if (db == null)
-            initializeDataBase();
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+    private void retreiveDB() {
+        myGroceryList.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        groceryList = document.getData();
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                        groceryList = new HashMap<>();
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        Map<String, Object> groceryList = new HashMap<>();
     }
 
-    public void testDBDelete(String entry){
-        if (db != null) {
-//            db.collection(entry).
+    public String generateListID() {
+        int id = currentListCountID;
+        id++;
+        currentListCountID = id;
+        return String.valueOf(id);
+    }
 
+    public int getRandomArrayEntry() {
+        Random rand = new Random();
+        return rand.nextInt(10);
+    }
+
+    public void testDBEntry() {
+//        Map<String, Object> groceryList = new HashMap<>();
+//        for (int i = 0; i < 15; i++) {
+//            groceryList.put(generateListID(), foodArray[getRandomArrayEntry()]);
+//        }
+//        myGroceryList.set(groceryList);
+        for (int i = 0; i < 15; i++) {
+            groceryList.put(generateListID(), foodArray[getRandomArrayEntry()]);
         }
+        myGroceryList.set(groceryList);
+
+    }
+
+    public void testDBDelete(String entry) {
     }
 
     @Override
